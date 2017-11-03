297077c3430e32819f70be569a2ddfe49b759e18
Subramani <subramani@qburst.com>
2017-11-03 16:35:27 +0530

    Notification - First Commit

---
+++ NotificationWebApplication/Hubs/NotificationHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using NotificationWebApplication.Models;
+
+namespace NotificationWebApplication.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        private static List<UserConnectionModel> _userList = new List<UserConnectionModel>();
+
+        public void ReceiveNewAndBroadCastAllUsers(string newUser)
+        {
+            if(newUser != string.Empty)
+                _userList.Add(new UserConnectionModel()
+                {
+                    ConnectionId = Context.ConnectionId,
+                    Name = newUser
+                });
+
+            Clients.All.BroadcastAllUsers(_userList);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            if(_userList.Any(x=>x.ConnectionId == Context.ConnectionId))
+            {
+                _userList.RemoveAll(x => x.ConnectionId == Context.ConnectionId);
+                Clients.All.BroadcastAllUsers(_userList);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ NotificationWebApplication/Models/UserConnectionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotificationWebApplication.Models
+{
+    public class UserConnectionModel
+    {
+        public string Name { get; set; }
+
+        public string ConnectionId { get; set; }
+    }
+}
