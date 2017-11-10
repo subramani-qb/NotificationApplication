705ca9cde1d246ecef811a1d3b35a4c51a98a886
Subramani <subramani@qburst.com>
2017-11-10 19:28:39 +0530

    Revert "NOTF-001 Changes committed."
    This reverts commit f3aeed161277981f9adc3c06d8beb8ef2f3a95b0.

---
+++ Notification.Models/UserTimeDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Models
+{
+    public class UserTimeDetailsModel
+    {
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; }
+        
+        public DateTime LastActiveTime { get; set; }
+    }
+}
---
+++ NotificationWebApplication/ActionFilters/UpdateTimeFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotificationWebApplication.ActionFilters
+{
+    public class UpdateTimeFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
